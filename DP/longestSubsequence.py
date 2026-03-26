c3a83d8f9e0d0ba8ce24a041737d382763445ade
Cemal Enes Durak <cenesdurak@gmail.com>
2026-03-26 18:59:17 -0400

    Added new Solution

---
+++ DP/longestSubsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        Solution
+        1 - state variable i is the ith element
+        2 - dp[i] returns the longest increasing subsequence ending at i
+        3 - recurrence relation: dp[i] = max(dp[j] + 1) for all j < i
+        4 - base case: all elements are 1 because all are starting of an increasing subarray 
+        5 - Final answer is max(dp)
+        """
+
+        dp = [1] * len(nums)
+        for i in range(len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp)
