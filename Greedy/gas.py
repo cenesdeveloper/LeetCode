989584076e452361538ccf72093b17de6339b088
Cemal Enes Durak <cenesdurak@gmail.com>
2026-06-27 11:17:40 -0400

    More logical greedy solution that checks total gas at the beginning

--- Greedy/gas.py
+++ Greedy/gas.py
@@ -5,15 +5,18 @@ class Solution:
         1 - check if sum(gas) >= sum(cost) because if that is false then it is not possible
         2 - since there is single solution if a starting point fails start from next one it is guarenteed to be found if exists
         """
-        curg = 0
-        totg = 0
+
+        tg = sum(gas) - sum(cost)
+        if tg < 0:
+            return -1
+
+        cg = 0
         res = 0
 
         for i in range(len(gas)):
-            totg += gas[i] - cost[i]
-            curg += gas[i] - cost[i]
+            cg += gas[i] - cost[i]
 
-            if curg < 0:
+            if cg < 0:
+                cg = 0
                 res = i + 1
-                curg = 0
-        return res if totg >= 0 else -1
+        return res
